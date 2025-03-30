5c247927984242b9f0f5f7b8b7ff41932014a20a
onelhounnouvi <159829867+onelhounnouvi@users.noreply.github.com>
2025-03-30 20:06:46 +0200

    Delete test_recuit_simule.py

--- test_recuit_simule.py
+++
@@ -1,9 +0,0 @@
-from recuit_simule import *
-
-corpus_ref = file_to_str("germinal_nettoye")
-dico_ngrams = normaliser_dico(dico_n_grammes(corpus_ref, 4))
-
-a_dechiffrer = file_to_str("chiffres/chiffre_germinal_52_1199_1")
-texte = recuit_simule(a_dechiffrer, 10000, dico_ngrams, 4, 0.01)
-
-str_to_file(texte, "resultat")
